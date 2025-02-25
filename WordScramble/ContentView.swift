aa8c045ada8e0b8a98fa92c82cf42afb480c46ff
GreenRamSilgen <kiranshrs@yahoo.com>
2025-02-25 14:14:03 -0800

    Initial commit with basic states

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,14 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("INITIAL")
     }
 }
 
