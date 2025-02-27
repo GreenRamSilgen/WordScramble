da5d044a99c5772b320a6ef6f8ebd896fdc8e110
GreenRamSilgen <kiranshrs@yahoo.com>
2025-02-26 18:09:22 -0800

    Added toolbar button to reset game. Added score. Updated answer validaiton to disallow original word and words with less than 3 letters.

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -11,9 +11,129 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
-
+    @State private var score = 0
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
-        Text("INITIAL")
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                        .onSubmit(addNewWord)
+                        .onAppear(perform: startGame)
+                        .alert(errorTitle, isPresented: $showingError) { } message: {
+                            Text(errorMessage)
+                        }
+                }
+                Section{
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }                }
+            }
+            .navigationTitle("\(score) - \(rootWord)")
+            .navigationBarTitleDisplayMode(.automatic)
+            .toolbar(content: {
+                Button("Reset") {
+                    startGame()
+                }
+            })
+            
+        }
+
+
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+
+        return true
+    }
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func isLong(word: String) -> Bool {
+        return word.count >= 3
+    }
+    
+    func isNotRootWord(word: String) -> Bool {
+        return word != rootWord
+    }
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL, encoding: .ascii) {
+                let allWords = startWords.components(separatedBy: "\n")
+                
+                rootWord = allWords.randomElement() ?? "silkWorm"
+                return
+            }
+        }
+    }
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        
+        guard answer.count > 0 else { return }
+        
+        guard isLong(word: answer) else {
+            wordError(title: "Word is too short", message: "Input a longer word")
+            return
+        }
+        
+        guard isNotRootWord(word: answer) else {
+            wordError(title: "That is original", message: "Take a real guess")
+            return
+        }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+            score += answer.count
+        }
+        newWord = ""
     }
 }
 
